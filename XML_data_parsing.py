c5f53092085a6da4273da6784c0185ffe163af89
hwane123 <hwane123@hwtechnet.com>
2022-11-12 17:55:44 -0500

    first commit

---
+++ XML_data_parsing.py
@@ -0,0 +1,10 @@
+import xmltodict
+
+with open("interface_G1.xml") as data:
+    interface = data.read()
+    
+xml_to_python = xmltodict.parse(interface)
+
+with open("xmlsample.xml", "w") as data:
+    data.write(xmltodict.unparse(xml_to_python, pretty=True))
+
